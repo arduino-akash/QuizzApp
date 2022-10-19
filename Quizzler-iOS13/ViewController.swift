76a16cf7b91ec72998a7c41fc985d24627984817
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-10-19 14:59:03 +0530

    Second Commit

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [ ["A slug's blood is green.", "True"],
+                 ["In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", "False"],
+                 ["No piece of square dry paper can be folded in half more than 7 times.", "True"]
+               ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle  //True || False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        }
+        else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+        questionNumber += 1
+        } else{
+            questionNumber = 0
+        }
+         
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
